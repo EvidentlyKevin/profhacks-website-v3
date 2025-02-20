566d9cd47b14c479aa42c5ca9254835332c357b8
Elisabeth Yap <ednamode2048@gmail.com>
2025-02-20 10:49:24 -0500

    added nosu.io to sponsor list

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -5,23 +5,29 @@ import { getImageUrl } from "../../utils";
 export const Sponsors = () => {
   return (
     <section className={styles.container} id="sponsors">
-        <h2 className={styles.title}>-Sponsors-</h2>
-        <h3>Thank you to all of our sponsors that helped out with the event!</h3>
-        <a href="https://engineering.rowan.edu/programs/electricalcomputer/" target="_blank">
+      <h2 className={styles.title}>-Sponsors-</h2>
+      <h3>Thank you to all of our sponsors that helped out with the event!</h3>
+      <a href="https://engineering.rowan.edu/programs/electricalcomputer/" target="_blank">
         <img
           src={getImageUrl("sponsors/ece.png")}
           alt="island"
           className={styles.aboutImage}
         /></a>
-        <a href="https://philadelphia.ieee.org/" target="_blank">
+      <a href="https://philadelphia.ieee.org/" target="_blank">
         <img
           src={getImageUrl("sponsors/philly.png")}
           alt="island"
           className={styles.aboutImage}
         /></a>
-        <a href="https://www.redbull.com/us-en/" target="_blank">
+      <a href="https://www.redbull.com/us-en/" target="_blank">
         <img
           src={getImageUrl("sponsors/redbull.png")}
+          alt="island"
+          className={styles.aboutImage}
+        /></a>
+      <a href="https://www.nosu.io/" target="_blank">
+        <img
+          src={getImageUrl("sponsors/nosu,io_enhanced.png")}
           alt="island"
           className={styles.aboutImage}
         /></a>
