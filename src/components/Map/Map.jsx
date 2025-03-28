a0cc25c0542ac5e99b93479d85394ff0afee5ce7
Elisabeth Yap <ednamode2048@gmail.com>
2025-03-28 13:05:55 -0400

    rehe

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,36 +1,31 @@
-import React from 'react'
+import React from 'react';
 import styles from "./Map.module.css";
-import { getImageUrl } from "../../utils";
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
 
 export const Maps = () => {
     const slideImages = [
-        getImageUrl("maps/Virtual_Map_1.png"),
-        getImageUrl("maps/Virtual_Map_2v2.png"),
-        getImageUrl("maps/Virtual_Map_3v3.png"),
-        
+        "/maps/Virtual_Map_1.png",
+        "/maps/Virtual_Map_2v2.png",
+        "/maps/Virtual_Map_3v3.png",
     ];
 
     return (
         <section className={styles.container} id="pictures">
-            <h2 className={styles.title}>-Rowan Engineering Building Maps-</h2>
-            <br></br>
+            <h2 className={styles.title}>- Rowan Engineering Building Maps -</h2>
             <div className={styles.slideContainer}>
-               
                 <Slide>
                     <div className={styles.fade}>
-                        <img src={slideImages[0]} />
+                        <img src={slideImages[0]} alt="Map 1" />
                     </div>
                     <div className={styles.fade}>
-                        <img src={slideImages[1]} />
+                        <img src={slideImages[1]} alt="Map 2" />
                     </div>
                     <div className={styles.fade}>
-                        <img src={slideImages[2]} />
+                        <img src={slideImages[2]} alt="Map 3" />
                     </div>
-                   
                 </Slide>
             </div>
         </section>
-    )
-}
+    );
+};
